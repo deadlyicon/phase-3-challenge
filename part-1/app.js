0bfe3001a15c8f01c23dccdd843b84043ddd4fef
zezemanolo <manoloish@gmail.com>
2017-07-25 11:09:21 -0700

    adds 3rd route

--- part-1/app.js
+++ part-1/app.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
 
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 app.get('/supported-operations', function(req, res) {
   var operations = {
@@ -14,8 +17,45 @@ app.get('/supported-operations', function(req, res) {
 
 app.get('/square?:number', function(req, res) {
   var result = {'result': req.query.number ** 2};
-  res.json(result);
+  res.send(result);
 });
+
+app.post('/compute', function(req, res, next) {
+  var total;
+  // var reqBody = {
+  //   "operator": "/",
+  //   "operands": [5,3]
+  // };
+
+    console.log(req.body);
+    var operands = JSON.parse(req.body.operands);
+    var operator = req.body.operator;
+
+    //maybe use regexp to remove brackets?
+
+    console.log("1st shit", operator);
+    console.log(operands[0]);
+    console.log(operands[1]);
+
+    switch(operator) {
+      case '/':
+        total = operands[0] / operands[1]
+        break;
+      case '+':
+        total = operands[0] + operands[1]
+        break;
+      case '-':
+        total = operands[0] - operands[1]
+        break;
+      case '*':
+        total = operands[0] * operands[1]
+        break;
+      default:
+        throw 'error": "invalid operator ?. Valid operators are /, +, -, *'
+    }
+  console.log("yolo", req.body);
+  res.json({'total': total});
+})
 
 app.listen(3000, function() {
   console.log('Server started!');
