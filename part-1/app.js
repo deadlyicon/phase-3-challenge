ccb50ae350b0e38b3619edb9ea65303c977ddd10
zezemanolo <manoloish@gmail.com>
2017-07-25 08:57:05 -0700

    fixes response in second route

--- part-1/app.js
+++ part-1/app.js
@@ -1,19 +1,20 @@
 var express = require('express');
 var app = express();
 
+
 app.get('/supported-operations', function(req, res) {
   var operations = {
     "/": "division",
     "+": "addition",
     "-": "subtration",
     "*": "multiplication"
-  }
-  res.json(operations)
+  };
+  res.json(operations);
 });
 
 app.get('/square?:number', function(req, res) {
-  var result = {'result': req.query.number ** 2}
-  res.send(result)
+  var result = {'result': req.query.number ** 2};
+  res.json(result);
 });
 
 app.listen(3000, function() {
